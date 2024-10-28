ca173ba4b329a9c9bc0b846f6d52ba5ecbfa468b
Егор <egor.lazeba@yandex.ru>
2024-10-28 17:08:36 +0300

    Минорные исправления

--- PaymentService/Services/Implementations/SubscriptionService.cs
+++ PaymentService/Services/Implementations/SubscriptionService.cs
@@ -7,209 +7,207 @@ using SubscriptionDBMongoAccessor;
 using SubscriptionDBMongoAccessor.Infrastracture;
 using SubscriptionsConfig = SubscriptionDBMongoAccessor.Infrastracture.SubscriptionsConfig;
 
-namespace PaymentService.Services.Implementations
+namespace PaymentService.Services.Implementations;
+
+public class SubscriptionService : ISubscriptionService
 {
-    public class SubscriptionService : ISubscriptionService
+    private readonly SubscriptionDBAccessor _dbAccessor;
+    private readonly PlansConfiguration _plans;
+    private readonly IUserService _userService;
+    private readonly IRecurrentServiceManager _recurrentServiceManager;
+    private readonly ILogger<SubscriptionService> _logger;
+        
+    private const string CancellationJobName = "CancellationSubscriptionsJob";
+        
+    public SubscriptionService(IOptions<DbSettings> dbSettings, IOptions<SubscriptionsConfig> subscriptionsConfig,
+        IOptions<PlansConfiguration> plans, IUserService userService,
+        IRecurrentServiceManager recurrentServiceManager,  ILogger<SubscriptionService> logger)
     {
-        private readonly SubscriptionDBAccessor _dbAccessor;
-        private readonly PlansConfiguration _plans;
-        private readonly IUserService _userService;
-        private readonly IRecurrentServiceManager _recurrentServiceManager;
-        private readonly ILogger<SubscriptionService> _logger;
-        
-        private const string CancellationJobName = "CancellationSubscriptionsJob";
-        
-        public SubscriptionService(IOptions<DbSettings> dbSettings, IOptions<SubscriptionsConfig> subscriptionsConfig,
-            IOptions<PlansConfiguration> plans, IUserService userService,
-            IRecurrentServiceManager recurrentServiceManager,  ILogger<SubscriptionService> logger)
-        {
-            _dbAccessor = new SubscriptionDBAccessor(dbSettings.Value, subscriptionsConfig.Value);
-            _plans = plans.Value;
-            _userService = userService;
-            _recurrentServiceManager = recurrentServiceManager;
-            _logger = logger;
+        _dbAccessor = new SubscriptionDBAccessor(dbSettings.Value, subscriptionsConfig.Value);
+        _plans = plans.Value;
+        _userService = userService;
+        _recurrentServiceManager = recurrentServiceManager;
+        _logger = logger;
             
-            RunCancellationJob();
+        RunCancellationJob();
+    }
+
+    public Task<Subscription> GetSubscription(string subId)
+    {
+        var subscription = _dbAccessor.GetSubscription(subId);
+        return subscription;
+    }
+
+    public async Task<string> CreateFreeSubscription(string userId, bool setupUsage, bool discardUsage = true)
+    {
+        var freePlanId = _plans.FreePlanId;
+
+        _recurrentServiceManager.CancelAllRecurrentJobByUserId(userId);
+
+        if (setupUsage && discardUsage)
+        {
+            await _dbAccessor.SetUpUsage(userId, freePlanId);
+        }
+        else
+        {
+            await _dbAccessor.SetUsage(userId, freePlanId);
         }
 
-        public Task<Subscription> GetSubscription(string subId)
-        {
-            var subscription = _dbAccessor.GetSubscription(subId);
-            return subscription;
-        }
+        var subscriptionId = await _dbAccessor.SetFreeSubscription(freePlanId);
 
-        public async Task<string> CreateFreeSubscription(string userId, bool setupUsage, bool discardUsage = true)
-        {
-            var freePlanId = _plans.FreePlanId;
+        return subscriptionId;
+    }
 
-            _recurrentServiceManager.CancelAllRecurrentJobByUserId(userId);
-
-            if (setupUsage && discardUsage)
-            {
-                await _dbAccessor.SetUpUsage(userId, freePlanId);
-            }
-            else
-            {
-                await _dbAccessor.SetUsage(userId, freePlanId);
-            }
-
-            var subscriptionId = await _dbAccessor.SetFreeSubscription(freePlanId);
-
-            return subscriptionId;
-        }
-
-        public async Task<string> SetSubscription(string userId, string planId, string subscriptionId)
-        {
-            await _dbAccessor.SetUsage(userId, planId);
+    public async Task<string> SetSubscription(string userId, string planId, string subscriptionId)
+    {
+        await _dbAccessor.SetUsage(userId, planId);
            
-            var result = await _userService.SetSubscription(userId, subscriptionId, false);
+        var result = await _userService.SetSubscription(userId, subscriptionId, false);
 
-            return subscriptionId;
-        }
+        return subscriptionId;
+    }
 
-        public async Task<string> CreateSubscription(string planId, string invoiceId, string? promocode)
-        {
-            var subscriptionId = string.Empty;
+    public async Task<string> CreateSubscription(string planId, string invoiceId, string? promocode)
+    {
+        var subscriptionId = string.Empty;
             
-            if (promocode != null)
-            {
-                var promoModel = await _dbAccessor.GetPromocode(promocode);
-                var plan = await _dbAccessor.GetPlan(planId);
-                var sale = CalculateSale(promoModel, plan);
-
-                subscriptionId = await _dbAccessor.CreateSubscription(planId, invoiceId, sale, promocode);
-            }
-            else
-            {
-                subscriptionId = await _dbAccessor.CreateSubscription(planId, invoiceId, 0, promocode);
-            }
-
-            return subscriptionId;
-        }
-
-        [Queue("usages")]
-        public async Task InsureSubscription(string userId, string planId)
-        {
-            var subId = await _userService.GetActiveSubscription(userId);
-
-            if (subId == null)
-            {
-                return;
-            }
-
-            var subscription = await GetSubscription(subId);
-            if (subscription.Status != SubscriptionStatus.Active)
-            {
-                await DeactivateSubscription(subId);
-                var freeSubID = await CreateFreeSubscription(userId, false);
-                await _userService.SetSubscription(userId, freeSubID, true);
-            }
-        }
-
-        public async Task<string> ActivateSubscription(string subscriptionId)
-        {
-            return await _dbAccessor.SetSubscriptionStatus(subscriptionId, SubscriptionStatus.Active);
-        }
-
-        public async Task<string> DeactivateSubscription(string subscriptionId)
-        {
-            return await _dbAccessor.SetSubscriptionStatus(subscriptionId, SubscriptionStatus.Inactive);
-        }
-
-        public async Task<bool> CheckInvoiceExist(int invoiceId)
-        {
-            return await _dbAccessor.CheckInvoiceExist(invoiceId);
-        }
-
-        public bool CancelAllUserRecurringJobs(string userId)
-        {
-            try
-            {
-                _recurrentServiceManager.CancelAllRecurrentJobByUserId(userId);
-            }
-            catch(Exception ex)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public async Task<BillingPromocode> GetPromocode(string promocode)
+        if (promocode != null)
         {
             var promoModel = await _dbAccessor.GetPromocode(promocode);
-
-            if (promoModel == null)
-            {
-                throw new ArgumentException($"Not found promocode {promocode}");
-            }
-            return promoModel;
-        }
-
-        public async Task<PriceModel> CalculatePrice(string planId, string promocode)
-        {
-            var promoModel = await _dbAccessor.GetPromocode(promocode);
-
             var plan = await _dbAccessor.GetPlan(planId);
-
             var sale = CalculateSale(promoModel, plan);
 
-            if(promoModel == null)
-            {
-                return new PriceModel
-                {
-                    Currency = plan.Currency,
-                    AmountTotal = (double)plan.Price
-                };
-            }    
+            subscriptionId = await _dbAccessor.CreateSubscription(planId, invoiceId, sale, promocode);
+        }
+        else
+        {
+            subscriptionId = await _dbAccessor.CreateSubscription(planId, invoiceId, 0, promocode);
+        }
 
+        return subscriptionId;
+    }
+
+    [Queue("usages")]
+    public async Task InsureSubscription(string userId, string planId)
+    {
+        var subId = await _userService.GetActiveSubscription(userId);
+
+        if (subId == null)
+        {
+            return;
+        }
+
+        var subscription = await GetSubscription(subId);
+        if (subscription.Status != SubscriptionStatus.Active)
+        {
+            await DeactivateSubscription(subId);
+            var freeSubID = await CreateFreeSubscription(userId, false);
+            await _userService.SetSubscription(userId, freeSubID, true);
+        }
+    }
+
+    public async Task<string> ActivateSubscription(string subscriptionId)
+    {
+        return await _dbAccessor.SetSubscriptionStatus(subscriptionId, SubscriptionStatus.Active);
+    }
+
+    public async Task<string> DeactivateSubscription(string subscriptionId)
+    {
+        return await _dbAccessor.SetSubscriptionStatus(subscriptionId, SubscriptionStatus.Inactive);
+    }
+
+    public async Task<bool> CheckInvoiceExist(int invoiceId)
+    {
+        return await _dbAccessor.CheckInvoiceExist(invoiceId);
+    }
+
+    public bool CancelAllUserRecurringJobs(string userId)
+    {
+        try
+        {
+            _recurrentServiceManager.CancelAllRecurrentJobByUserId(userId);
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<BillingPromocode> GetPromocode(string promocode)
+    {
+        var promoModel = await _dbAccessor.GetPromocode(promocode);
+
+        if (promoModel == null)
+        {
+            throw new ArgumentException($"Not found promocode {promocode}");
+        }
+        return promoModel;
+    }
+
+    public async Task<PriceModel> CalculatePrice(string planId, string promocode)
+    {
+        var promoModel = await _dbAccessor.GetPromocode(promocode);
+
+        var plan = await _dbAccessor.GetPlan(planId);
+
+        var sale = CalculateSale(promoModel, plan);
+
+        if(promoModel == null)
+        {
             return new PriceModel
             {
-                Promocode = promoModel.Promocode,
-                AmountTotal = (double)plan.Price - (double)sale,
-                DiscountType = promoModel.DiscountType,
-                DiscountValue = promoModel.DiscountValue,
                 Currency = plan.Currency,
-                DiscountCurrencyValue = (double)sale,
+                AmountTotal = (double)plan.Price
             };
-        }
+        }    
 
-        private void RunCancellationJob()
+        return new PriceModel
         {
-            RecurringJob.AddOrUpdate(CancellationJobName, () => CancelExpiredSubscriptions(),
-                Cron.Minutely);
-        }
+            Promocode = promoModel.Promocode,
+            AmountTotal = (double)plan.Price - (double)sale,
+            DiscountType = promoModel.DiscountType,
+            DiscountValue = promoModel.DiscountValue,
+            Currency = plan.Currency,
+            DiscountCurrencyValue = (double)sale,
+        };
+    }
 
-        public async Task CancelExpiredSubscriptions()
-        {
-            var subscriptions = await _dbAccessor.GetSubscriptions(SubscriptionStatus.Pending);
+    private void RunCancellationJob()
+    {
+        RecurringJob.AddOrUpdate(CancellationJobName, () => CancelExpiredSubscriptions(), Cron.Minutely);
+    }
+
+    public async Task CancelExpiredSubscriptions()
+    {
+        var subscriptions = await _dbAccessor.GetSubscriptions(SubscriptionStatus.Pending);
             
-            foreach (var subscription in subscriptions)
-            {
-                if (subscription.CancellationTime > DateTime.Now || subscription.Id == null) continue;
-                await _dbAccessor.SetSubscriptionStatus(subscription.Id, SubscriptionStatus.Failed);
-                _logger.LogInformation($"Subscription {subscription.Id} has been cancelled.");
-            }
-        }
-        
-        private decimal CalculateSale(BillingPromocode? promocode, Plan plan)
+        foreach (var subscription in subscriptions)
         {
-            decimal sale = 0;
-
-            if(promocode == null)
-            {
-                return 0;
-            }
-
-            if (promocode.DiscountType == BillingPromocodeDiscountType.Percent)
-            {
-                sale = Math.Round(plan.Price / 100 * (decimal)promocode.DiscountValue!, 2);
-            }
-            else
-            {
-                sale = (decimal)promocode.DiscountValue!;
-            }
-
-            return sale;
+            if (subscription.CancellationTime > DateTime.Now || subscription.Id == null) continue;
+            await _dbAccessor.SetSubscriptionStatus(subscription.Id, SubscriptionStatus.Failed);
+            _logger.LogInformation("Subscription {id} has been cancelled", subscription.Id);
         }
+    }
+        
+    private decimal CalculateSale(BillingPromocode? promocode, Plan plan)
+    {
+        decimal sale = 0;
+
+        if(promocode == null)
+        {
+            return 0;
+        }
+
+        if (promocode.DiscountType == BillingPromocodeDiscountType.Percent)
+        {
+            sale = Math.Round(plan.Price / 100 * (decimal)promocode.DiscountValue!, 2);
+        }
+        else
+        {
+            sale = (decimal)promocode.DiscountValue!;
+        }
+
+        return sale;
     }
 }
